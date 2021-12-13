b58d8de00d3d33438cc0e28b9476235ade74ccd9
Jakub Ryba <31319185+The-EmptyName@users.noreply.github.com>
2021-12-13 11:04:18 +0100

    Day 13

---
+++ Day 13/1.py
@@ -0,0 +1,42 @@
+#file = open("test.txt")
+file = open("input.txt")
+
+paper = [i.strip() for i in file]
+instructions = [paper[i] for i in range(paper.index("")+1, len(paper))]
+dots = [paper[i] for i in range(paper.index(""))]
+
+def fold_y(y, dots): # int y, str[] dots
+    for i in range(len(dots)):
+        if int(dots[i].split(",")[1]) > y:
+            dots[i] = dots[i].split(",")[0] + "," + str( int(dots[i].split(",")[1]) - 2 * (int(dots[i].split(",")[1])-y) )
+
+def fold_x(x, dots): # int x, str[] dots
+    for i in range(len(dots)):
+        if int(dots[i].split(",")[0]) > x:
+            dots[i] = str( int(dots[i].split(",")[0]) - 2 * (int(dots[i].split(",")[0])-x) ) + "," + dots[i].split(",")[1]
+
+#fold_y(7, dots)
+#fold_x(5, dots)
+
+first = (instructions[0].split(" ")[-1]).split("=")
+if first[0] == "x":
+    fold_x(int(first[1]), dots)
+else:
+    fold_y(int(first[1]), dots)
+
+print(len(set(dots)))
+
+def display_test(dots):
+    for y in range(7):
+        for x in range(15):
+            dot = False
+            for i in range(len(dots)):
+                if int(dots[i].split(",")[0]) == x and int(dots[i].split(",")[1]) == y:
+                    print("#", end = "")
+                    dot = True
+                    break
+            if not dot:
+                print(".", end = "")
+        print()
+
+#display_test(dots)
